8b4764796c851a2c1bfefd366fa99c5c77ad44a8
Nem Sothea <NemSothea@NemSothea.local>
2017-03-09 13:39:38 +0700

    Initial Commit

---
+++ ObjectInitializer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectInitializer
+//
+//  Created by Nem Sothea on 3/9/17.
+//  Copyright © 2017 Nem Sothea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
